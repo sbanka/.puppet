d860359694dbc8efe94cb0a5728ebc58a56bd6c2
Szymon Banka <sbanka@mirantis.com>
2013-12-27 17:26:37 +0100

    fix

--- modules/zsh_config/manifests/init.pp
+++ modules/zsh_config/manifests/init.pp
@@ -2,7 +2,7 @@ class zsh_config {
 
   exec { 'create-control-exec':
     command  => "wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh",
-    path     => ['/usr/local/bin']
+    path     => ['/usr/local/bin', '/bin', '/usr/bin']
   }
 
 }
