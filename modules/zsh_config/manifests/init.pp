cfb37cc73485cfd8ab6ca72f49bf576ab89d3454
Szymon Banka <sbanka@mirantis.com>
2013-12-27 17:59:13 +0100

    fix

--- modules/zsh_config/manifests/init.pp
+++ modules/zsh_config/manifests/init.pp
@@ -1,8 +1,13 @@
 class zsh_config {
 
-  exec { 'create-control-exec':
-    command  => "wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh",
+  # exec { 'download-ohmyzsh':
+  #   command  => "wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh",
+  #   path     => ['/usr/local/bin', '/bin', '/usr/bin']
+  # }
+  
+  exec { 'download-ohmyzsh':
+    command  => "git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh",
     path     => ['/usr/local/bin', '/bin', '/usr/bin']
   }
-
+  
 }
